a052b75687630cca0edb34f101178fe3611f480c
Jaden Rotter <jaden.rotter@tum.de>
2025-11-02 14:21:11 +0100

    remove old files

--- distance_kernels/distance_parse.cpp
+++
@@ -1,8 +0,0 @@
-#include "distance_kernels.h"
-DistanceType parse_distance_type(const std::string& arg) {
-    if (arg == "l2") return L2;
-    if (arg == "l1") return L1;
-    if (arg == "cosine") return COSINE;
-    throw std::runtime_error("Unknown distance type");
-}
-
